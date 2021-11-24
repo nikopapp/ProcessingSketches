168c5f6b4a3c247a459d276c3289cdd3dc7fe878
nikopapp <papakos1@gmail.com>
2021-11-24 10:16:39 +0000

    augment

--- src/main/java/com/nikpappas/sketch/fractal/Mandelbrot.java
+++ src/main/java/com/nikpappas/sketch/fractal/Mandelbrot.java
@@ -1,25 +1,22 @@
 package com.nikpappas.sketch.fractal;
 
+import com.nikpappas.utils.collection.Pair;
 import processing.core.PApplet;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Mandelbrot extends PApplet {
-    private static final int NUM_OF_THREADS = 2;
+    private static final int NUM_OF_THREADS = 10;
+    private static final int MAX_SIZE_OF_POINTS = 10000000;
     private List<ComplexAny> toDraw;
+    private List<Thread> threads;
+    private boolean nearMouse = true;
+    private float mouseRange = 0.1f;
+    float scale;
 
-    Runnable findBrots = () -> {
-        System.out.println("StartedBroting");
-        while (true) {
-            ComplexAny c = ComplexBD.of(random(-2, 2), random(-2, 2));
-            if (MandelbrotSet.contains(c)) {
-                toDraw.add(c);
-                System.out.println(toDraw.size() + " - " + c + ": Is mandel");
-            }
-        }
-    };
 
     public static void main(String[] args) {
         PApplet.main(Thread.currentThread().getStackTrace()[1].getClassName());
@@ -28,44 +25,72 @@ public class Mandelbrot extends PApplet {
     @Override
     public void settings() {
         size(900, 800);
+        scale = (min(width, height) * (1f - 0.2f)) / 4f;
         toDraw = new ArrayList<>();
-        IntStream.range(0, NUM_OF_THREADS)
-                .mapToObj(x -> findBrots)
-                .map(Thread::new)
-                .forEach(Thread::start);
+        Runnable findBrots = () -> {
+            System.out.println("StartedBroting");
+            while (!Thread.currentThread().isInterrupted()) {
+
+                ComplexAny c;
+                if (nearMouse) {
+                    c = Complex.of((mouseX-width/2f) / scale + random(-mouseRange, mouseRange), (height/2f -mouseY) / scale + random(-mouseRange, mouseRange));
+                } else {
+                    c = Complex.of(random(-2, 2), random(-2, 2));
+                }
+                if (MandelbrotSet.contains(c)) {
+                    toDraw.add(c);
+//                System.out.println(toDraw.size() + " - " + c + ": Is mandel");
+                }
+            }
+        };
+
+        threads = IntStream.range(0, NUM_OF_THREADS)
+                .mapToObj(x -> Pair.of(x, findBrots))
+                .map(x -> new Thread(x._2, "T" + x._1))
+                .collect(Collectors.toList());
+        threads.forEach(Thread::start);
     }
 
     @Override
     public void setup() {
+        frameRate(60);
         background(55);
     }
 
     @Override
     public void draw() {
-        background(55);
+        background(130);
+        int toDrawSize = toDraw.size();
+        text(toDrawSize, 10, 10);
         pushMatrix();
         translate(width / 2, height / 2);
 
-
-        float scale = 140f;
-        stroke(144);
-        noFill();
-        circle(0, 0, scale * 4);
+        // 20% margin
         noStroke();
-        fill(255);
-        int i = 1;
-        for (ComplexAny x : toDraw) {
+        fill(55);
+        // padding 10%
+        circle(0, 0, scale * 4.5f);
+        fill(255, 60);
+        for (int i = 0; i < toDrawSize; i++) {
+            ComplexAny x = toDraw.get(i);
             float coordX = (float) (scale * x.real());
             float coordY = (float) -(scale * x.imaginary());
-            circle(coordX, coordY, 5);
-            text(i++, coordX - 5, coordY - 5);
+            circle(coordX, coordY, 1);
+//            text(i+1, coordX - 5, coordY - 5);
         }
-
-        delay(100);
-
-
+        if (toDrawSize > MAX_SIZE_OF_POINTS) {
+            interrupt();
+        }
         popMatrix();
     }
 
 
+    void interrupt() {
+        threads.stream()
+                .filter(Thread::isAlive)
+                .forEach(x -> {
+                    x.interrupt();
+                    System.out.println("interrrupting " + x.getName());
+                });
+    }
 }
